7f02b3cff8641c27a950fa1e90680886ba076000
Marcos Felipe <omarkdev@gmail.com>
2016-11-04 23:29:56 -0200

    On enter send to /chat

--- app/Controllers/WelcomeController.js
+++ app/Controllers/WelcomeController.js
@@ -1,12 +1,14 @@
 $app.controller("WelcomeController", [
-    '$scope', 'UserFactory',
-    function($scope, UserFactory){
+    '$scope', 'UserFactory', '$location',
+    function($scope, UserFactory, $location){
         $scope.userLogin = function(){
             var nickUser = $scope.user.nick;
 
             UserFactory.set({
                 nick: nickUser
             });
+
+            return $location.path('/chat');
         }
     }
 ]);
