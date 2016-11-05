70615f86051af11279c655c5c443d8789e3f3d22
Marcos Felipe <omarkdev@gmail.com>
2016-11-04 22:45:21 -0200

    Create task js-vendor

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect');
+
+gulp.task('js-vendor', function(){
+    var filesVendor = [
+        './node_modules/angular/angular.min.js',
+        './node_modules/angular-route/angular-route.min.js',
+        './node_modules/angularfire/dist/angularfire.min.js',
+    ];
+
+    return gulp.src(filesVendor)
+        .pipe(concat('vendor-concated.js'))
+        .pipe(rename('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/assets/js/'));
+});
