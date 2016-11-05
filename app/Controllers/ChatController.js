4e934a5c89ead8c317f1c9d5a5a575b161825aff
Marcos Felipe <omarkdev@gmail.com>
2016-11-05 00:04:42 -0200

    Retrieving messages

--- app/Controllers/ChatController.js
+++ app/Controllers/ChatController.js
@@ -1,12 +1,16 @@
 $app.controller('ChatController', [
-    '$scope', 'UserFactory', '$location',
-    function($scope, UserFactory, $location){
+    '$scope', 'UserFactory', '$location', '$firebaseArray',
+    function($scope, UserFactory, $location, $firebaseArray){
         var userInfo = UserFactory.get();
 
-        if(!userInfo || !userInfo.nick){
+        if(!userInfo || !userInfo.nick)
             return $location.path('/');
-        }
 
         $scope.user = userInfo;
+
+        var ref = firebase.database().ref().child('messages');
+
+        var messages = $firebaseArray(ref);
+        $scope.messages = messages;
     }
 ]);
