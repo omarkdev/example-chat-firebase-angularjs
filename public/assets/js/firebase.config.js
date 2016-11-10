8dd2fc7d4a042da5e42543ed74950c8b905fd5a1
Marcos Felipe <omarkdev@gmail.com>
2016-11-10 03:15:31 -0200

    Changed nome _firebase.config.js

---
+++ public/assets/js/firebase.config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyB8Du1rZFIwpn3Og--xrKgTGSI1yZ5ZvgA",
+    authDomain: "chat-a3280.firebaseapp.com",
+    databaseURL: "https://chat-a3280.firebaseio.com",
+    storageBucket: "chat-a3280.appspot.com",
+    messagingSenderId: "640810308724"
+  };
+firebase.initializeApp(config);
